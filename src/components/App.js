a1cffc24639290119900063a9b02a81cd0e3dcf4
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-15 13:04:34 +0530

    Refactor App component with routing and navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import PostsPage from './components/PostsPage';
+import UsersPage from './components/UsersPage';
+import NotificationsPage from './components/NotificationsPage';
+import PostDetails from './components/PostDetails';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+export default function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <div className="App">
+      <nav>
+        <h1>GenZ</h1>
+        <div className="navlinks">
+          <Link to="/">Posts</Link>
+          <Link to="/users">Users</Link>
+          <Link to="/notifications">Notifications</Link>
+        </div>
+      </nav>
 
-export default App
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<PostsPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/posts/:postId" element={<PostDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
