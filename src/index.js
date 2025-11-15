0b82f4ab1f1cfc5c647875824cce1e8ceacf57a0
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-15 12:08:03 +0530

    Refactor index.js for React 18 and add Redux
    Updated ReactDOM rendering to use createRoot and added Redux and React Router integration.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import store from './store';
+import './index.css';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
