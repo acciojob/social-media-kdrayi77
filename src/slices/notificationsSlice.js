141649f6a9ce9b788dcbf2e378388effc3c4ed88
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-15 13:02:37 +0530

    Create notificationsSlice for managing notifications

---
+++ src/slices/notificationsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: { items: [] },
+  reducers: {
+    setNotifications(state, action) {
+      state.items = action.payload;
+    }
+  }
+});
+
+export const { setNotifications } = notificationsSlice.actions;
+export default notificationsSlice.reducer;
