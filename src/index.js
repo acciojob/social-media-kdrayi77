6500c9c7be611b312b4d71f30410b232317ad419
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-15 11:56:13 +0530

    Refactor ReactDOM rendering and imports
    Updated import statements and changed rendering method.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import store from './store';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
